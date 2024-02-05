cdbb9c5243b57147f115e0b959e2a6cc7f750f87
Jovan Mitić <67793349+Joxy97@users.noreply.github.com>
2024-02-05 02:33:27 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-python -m pip install --user virtualenv
+python3 -m pip install --user virtualenv
 virtualenv ai1_tapte_env
 source ai1_tapte_env/bin/activate
 
+python3 -m pip install --upgrade pip
 pip3 install -e .
 pip3 install torch torchvision torchaudio
